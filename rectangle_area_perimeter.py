cd1402f439907395320971324b4f0743e26489e4
Ubuntu <ubuntu@ip-172-31-27-134.ec2.internal>
2019-09-13 18:43:29 +0000

    program add

---
+++ rectangle_area_perimeter.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Created by: Wajd Mariam
+# Created on: September 2019
+# This program is to calculate the area and the perimeter of the rectangle
+
+
+def main():
+    print("If the rectangle dimensions are:")
+    print("3m wide 5m long")
+    print("")
+    print("The area is {} m^2" .format(3*5))
+    print("The perimeter is {}" .format(2*(3+5)))
+
+
+if __name__ == "__main__": 
+    main()
